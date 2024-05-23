0694fe822c4c9f8a310cbd776199876fe20999ff
Ubuntu <ubuntu@ip-172-31-13-244.ap-northeast-2.compute.internal>
2024-05-23 06:13:58 +0000

    Seperate non-map compnents from the app page

---
+++ src/components/Footer.svelte
@@ -0,0 +1,24 @@
+<!-- src/components/Footer.svelte -->
+
+<footer class="mt-8 text-center bg-gray-100 py-4">
+    <p class="text-gray-600">
+        View the source code on <a
+            href="https://github.com/seoulcity/sveltekit_tailwindcss_supabase_bolierplate"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="text-blue-600 hover:underline">GitHub</a>
+    </p>
+    <p class="text-gray-600 text-sm mt-2">
+        &copy; 2023 Kim JungHyun (<a
+            href="https://linkedin.com/in/seoulite"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="text-blue-600 hover:underline">LinkedIn</a
+        >) | Licensed under
+        <a
+            href="https://creativecommons.org/licenses/by/4.0/"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="text-blue-600 hover:underline">CC BY 4.0</a>
+    </p>
+</footer>
---
+++ src/components/Instruction.svelte
@@ -0,0 +1,18 @@
+<!-- src/components/Instruction.svelte -->
+<aside class="md:w-1/3 lg:w-1/4 bg-white p-6 rounded-lg shadow-md mb-8 md:mb-0 md:mr-8">
+    <h1 class="text-3xl font-bold mb-4">테크기업 찾기 지도</h1>
+    <p class="text-lg text-gray-600 mb-4">구글맵 사용법:</p>
+    <ul class="list-disc list-inside text-gray-600 mb-4">
+        <li>레이어: 좌측 상단의 레이어 버튼을 사용하여 다양한 정보를 추가하세요.</li>
+        <li>위치 검색: 지도 상단의 검색창을 이용해 원하는 위치를 검색하세요.</li>
+        <li>길찾기: 특정 위치를 선택하고 길찾기 버튼을 클릭하세요.</li>
+        <li>즐겨찾기 추가: 원하는 장소를 즐겨찾기에 추가하여 쉽게 찾을 수 있습니다.</li>
+    </ul>
+    <p class="text-lg text-gray-600 mb-4">How to use Google Maps:</p>
+    <ul class="list-disc list-inside text-gray-600">
+        <li>Layers: Use the layers button on the top left to add various information.</li>
+        <li>Location Search: Use the search box at the top of the map to find the desired location.</li>
+        <li>Directions: Select a specific location and click the directions button.</li>
+        <li>Add to Favorites: Add desired places to your favorites for easy access.</li>
+    </ul>
+</aside>
